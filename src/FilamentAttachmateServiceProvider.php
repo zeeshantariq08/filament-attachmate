9015bcbf4c3e099b92c7f7c85f4da2241c454dc9
Chris Jones <leeked@gmail.com>
2025-01-14 07:49:08 -0500

    refactor: update from package skeleton

--- src/FilamentAttachmateServiceProvider.php
+++ src/FilamentAttachmateServiceProvider.php
@@ -4,15 +4,55 @@ namespace ZeeshanTariq\FilamentAttachmate;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 
 class FilamentAttachmateServiceProvider extends PackageServiceProvider
 {
+    public static string $name = 'filament-attachmate';
+
+    public static string $viewNamespace = 'filament-attachmate';
 
     public function configurePackage(Package $package): void
     {
-        $package
-            ->name('filament-attachmate')
-            ->hasMigration('create_attachments_table')
-            ->runsMigrations();
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package->name(static::$name)
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->publishConfigFile()
+                    ->publishMigrations()
+                    ->askToRunMigrations();
+            });
+
+        $configFileName = $package->shortName();
+
+        if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
+            $package->hasConfigFile();
+        }
+
+        if (file_exists($package->basePath('/../database/migrations'))) {
+            $package->hasMigrations($this->getMigrations());
+        }
+
+        if (file_exists($package->basePath('/../resources/lang'))) {
+            $package->hasTranslations();
+        }
+
+        if (file_exists($package->basePath('/../resources/views'))) {
+            $package->hasViews(static::$viewNamespace);
+        }
+    }
+
+    /**
+     * @return array<string>
+     */
+    protected function getMigrations(): array
+    {
+        return [
+            'create_attachments_table',
+        ];
     }
 }
